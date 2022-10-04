e6562e1b341ab23621e5d73c78e8bea11abeaf02
NUCAL <yagmur.sahin@nucal.com>
2022-10-04 09:28:14 +0300

    NUCAL-UI | Front-end application initial commit, powered by NUCAL development team.

---
+++ src/components/pages/Screen1.vue
@@ -0,0 +1,105 @@
+<template>
+  <NucGridPage v-bind:gap="{'col':0,'row':0}" v-bind:rows="[1]"
+    v-bind:schema="{'r0-c0':{'col':0,'row':0,'showIn':['xs','sm','md','lg'],'colSpan':1,'rowSpan':1}}"
+    v-bind:columns="[1]" overflow="auto" v-bind:designMode="false" v-bind:minNsLimit="1301"
+    v-bind:style="{'background':'#FFFFFF','paddingTop':'0px','paddingLeft':'0px','paddingRight':'0px','paddingBottom':'0px'}">
+    <NucGridCell v-bind:wrap="true" v-bind:reverse="false" overflow="hidden" direction="column"
+      v-bind:showInDevice="['xs','sm','md','lg']" verticalAlignment="top" horizontalAlignment="left"
+      id="8fa68a98-7f21-40b2-9b7f-2b714ea3966c" v-bind:col="0" v-bind:row="0" v-bind:colSpan="1" v-bind:rowSpan="1"
+      v-bind:marginTop="0" v-bind:marginLeft="0" v-bind:marginRight="0" v-bind:marginBottom="0"
+      v-bind:style="{'height':'auto','background':'transparent','paddingTop':'0px','borderColor':'','borderStyle':'solid','borderWidth':'0px','paddingLeft':'0px','borderRadius':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'0px','borderLeftWidth':'0px','borderRightWidth':'0px','borderBottomWidth':'0px','borderTopLeftRadius':'1px','borderTopRightRadius':'1px','borderBottomLeftRadius':'1px','borderBottomRightRadius':'1px'}"
+      slot="r0-c0" area="r0-c0">
+      <NucGroupElement top="0%" left="0.13869625520110956%" v-bind:snapX="false" v-bind:snapY="false"
+        v-bind:isLock="false" bgColor="" position="absolute" ncTooltipText="" id="0d539c3e-9dab-4f61-991e-7a8db3f60f73"
+        v-bind:style="{'color':'#FFFFFF','width':'1263.5px','height':'766.2225px','zIndex':2,'fontSize':'10.153125px','maxWidth':'','minWidth':'6.76875','tabIndex':'-0.6768749999999999','boxShadow':'','fontStyle':'normal','marginTop':'0px','maxHeight':'','minHeight':'6.76875','textAlign':'center','background':'#e0e5eb','fontFamily':'Roboto-Regular,Roboto','fontWeight':'normal','lineHeight':'0.84609375','marginLeft':'0px','paddingTop':'','textShadow':'','visibility':'visible','borderColor':'#767676','borderStyle':'solid','marginRight':'0px','paddingLeft':'','marginBottom':'0px','paddingRight':'','paddingBottom':'','borderTopWidth':'0.6768749999999999px','textDecoration':'none','borderLeftWidth':'0.6768749999999999px','borderRightWidth':'0.6768749999999999px','borderBottomWidth':'0.6768749999999999px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px'}">
+        <WBoxColorAdvertisement top="22.55569221739468%" left="0%" v-bind:blank="true" v-bind:snapX="false"
+          v-bind:snapY="false" imgSrc="" v-bind:overlay="true" cardText="" position="absolute" bgVariant="primary"
+          changeColor="#f7743d" textVariant="white"
+          v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':false,'structure':[{'type':'string','propName':'imgSrc','displayName':'ImageUrl'},{'type':'string','propName':'cardText','displayName':'Value'}]}"
+          verticalPosition="align-items-center" horizontalPosition="justify-content-center"
+          id="3300cf5a-fe9e-4e52-83e4-18a297c1ed62"
+          v-bind:style="{'width':'1301.630625px','height':'219.984375px','zIndex':4,'maxWidth':'','minWidth':'','marginTop':'0px','maxHeight':'','minHeight':'','marginLeft':'0px','paddingTop':'0px','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px'}"/>
+        <WCircleImage top="0.6790125811541522%" left="19.23611323038737%" v-bind:blank="false" v-bind:snapX="false"
+          v-bind:snapY="false" rounded="circle" stretch="none"
+          imageUrl="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/164ce8aba7cc5e98824fe5fa/nn.jpg"
+          position="absolute" v-bind:isEnabled="true" v-bind:thumbnail="false" blankColor="black"
+          v-bind:aspectRatio="true" borderWidth="12px" dataLoading="static"
+          ncTooltipText="Please write a text to display in tooltip..."
+          v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':true,'structure':[{'type':'string','update':{'type':'dataModelForDynamicType'},'propName':'imageUrl','displayName':'Value'}]}"
+          ncClickBorderColor="black" ncHoverBorderColor="black" dataModelForDynamicType="string"
+          id="9a03a575-c15d-4e0e-8ce8-207add85c691"
+          v-bind:style="{'width':'487.34999999999997px','height':'490.0575px','zIndex':5,'maxWidth':'','minWidth':'','boxShadow':'','marginTop':'0px','maxHeight':'','minHeight':'','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','borderColor':'#FFFFFF','borderStyle':'solid','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','borderTopWidth':'8.122499999999999px','borderLeftWidth':'8.122499999999999px','borderRightWidth':'8.122499999999999px','borderBottomWidth':'8.122499999999999px','borderTopLeftRadius':'0px','borderTopRightRadius':'0px','borderBottomLeftRadius':'0px','borderBottomRightRadius':'0px'}"/>
+        <WBoxColorAdvertisement top="26.528613808811972%" left="53.399288659044814%" v-bind:blank="true"
+          v-bind:snapX="false" v-bind:snapY="false" imgSrc="" v-bind:overlay="true" cardText="" position="absolute"
+          bgVariant="primary" changeColor="#FFFFFF" textVariant="white"
+          v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':false,'structure':[{'type':'string','propName':'imgSrc','displayName':'ImageUrl'},{'type':'string','propName':'cardText','displayName':'Value'}]}"
+          verticalPosition="align-items-center" horizontalPosition="justify-content-center" v-bind:isLocked="false"
+          id="64eccebe-e131-4dc1-88a0-de1533a27f80"
+          v-bind:style="{'width':'372.95812499999994px','height':'352.65187499999996px','zIndex':6,'maxWidth':'','minWidth':'','marginTop':'0px','maxHeight':'','minHeight':'','marginLeft':'0px','paddingTop':'0px','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px'}"/>
+        <NucGroupElement top="69.02411812199547%" left="20.043836608442223%" v-bind:snapX="false" v-bind:snapY="false"
+          bgColor="" position="absolute" ncTooltipText="" id="a110ec5b-188c-499f-a369-31ed4226465f"
+          v-bind:style="{'color':'#FFFFFF','width':'522px','height':'219px','zIndex':7,'fontSize':'10.153125px','maxWidth':'','minWidth':'6.76875','tabIndex':'-0.6768749999999999','boxShadow':'','fontStyle':'normal','marginTop':'0px','maxHeight':'','minHeight':'6.76875','textAlign':'center','background':'#f7743d','fontFamily':'Roboto-Regular,Roboto','fontWeight':'normal','lineHeight':'0.84609375','marginLeft':'0px','paddingTop':'','textShadow':'','visibility':'visible','borderColor':'#767676','borderStyle':'none','marginRight':'0px','paddingLeft':'','marginBottom':'0px','paddingRight':'','paddingBottom':'','borderTopWidth':'0.6768749999999999px','textDecoration':'none','borderLeftWidth':'0.6768749999999999px','borderRightWidth':'0.6768749999999999px','borderBottomWidth':'0.6768749999999999px','borderTopLeftRadius':'27.075px','borderTopRightRadius':'27.075px','borderBottomLeftRadius':'27.075px','borderBottomRightRadius':'27.075px'}">
+          <WLabel top="8.663930567988062%" left="0%" text="Vurur yüze ifadesi APPime hoşgeldin bitanesi"
+            v-bind:snapX="true" v-bind:snapY="false" theme="theme--light01" position="absolute" ncTypography="custom"
+            ncTooltipText=""
+            v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':false,'structure':[{'type':'string','propName':'text','displayName':'Value'}]}"
+            id="a2f341e5-2553-4d6f-9043-89f1cc53df40"
+            v-bind:style="{'color':'#ffffff','width':'711px','height':'120px','zIndex':9,'fontSize':'32.489999999999995px','maxWidth':'','minWidth':'','fontStyle':'normal','marginTop':'0px','maxHeight':'','minHeight':'','textAlign':'left','fontFamily':'OpenSans-Bold,Open Sans','fontWeight':'normal','lineHeight':'0.9476249999999998','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','textDecoration':'none'}"/>
+        </NucGroupElement>
+        <NucGroupElement top="78.45679389105902%" left="69.96527989705403%" v-bind:snapX="false" v-bind:snapY="false"
+          bgColor="" position="absolute" ncTooltipText="" id="02490fc1-817e-40e6-8672-64f9ac5cdbd5"
+          v-bind:style="{'color':'#FFFFFF','width':'125.221875px','height':'125.221875px','zIndex':8,'fontSize':'10.153125px','maxWidth':'','minWidth':'6.76875','tabIndex':'-0.6768749999999999','boxShadow':'','fontStyle':'normal','marginTop':'0px','maxHeight':'','minHeight':'6.76875','textAlign':'center','background':'#f7743d','fontFamily':'Roboto-Regular,Roboto','fontWeight':'normal','lineHeight':'0.84609375','marginLeft':'0px','paddingTop':'','textShadow':'','visibility':'visible','borderColor':'#767676','borderStyle':'none','marginRight':'0px','paddingLeft':'','marginBottom':'0px','paddingRight':'','paddingBottom':'','borderTopWidth':'0.6768749999999999px','textDecoration':'none','borderLeftWidth':'0.6768749999999999px','borderRightWidth':'0.6768749999999999px','borderBottomWidth':'0.6768749999999999px','borderTopLeftRadius':'67.6875px','borderTopRightRadius':'67.6875px','borderBottomLeftRadius':'67.6875px','borderBottomRightRadius':'67.6875px'}"/>
+        <WLabel top="32.466881859634306%" left="4.323970068571923%" text="African Safari Tours &amp;"
+          v-bind:snapX="false" v-bind:snapY="false" theme="theme--light01" position="absolute" ncTypography="custom"
+          ncTooltipText=""
+          v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':false,'structure':[{'type':'string','propName':'text','displayName':'Value'}]}"
+          id="33634ac9-778a-473b-b3c4-2562477b40e0"
+          v-bind:style="{'color':'#000000','width':'','height':'','zIndex':9,'fontSize':'27.075px','maxWidth':'','minWidth':'','fontStyle':'normal','marginTop':'0px','maxHeight':'','minHeight':'','textAlign':'left','fontFamily':'Montserrat-SemiBold,Montserrat','fontWeight':'normal','lineHeight':'0.9476249999999998','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','textDecoration':'none'}"/>
+        <WLabel top="37.71605032461661%" left="54.7569465637207%" text="Holidays" v-bind:snapX="false"
+          v-bind:snapY="false" theme="theme--light01" position="absolute" ncTypography="custom" ncTooltipText=""
+          v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':false,'structure':[{'type':'string','propName':'text','displayName':'Value'}]}"
+          id="ae05e840-d582-4de5-8e93-596dd5bbd5cd"
+          v-bind:style="{'color':'#000000','width':'','height':'','zIndex':10,'fontSize':'27.075px','maxWidth':'','minWidth':'','fontStyle':'normal','marginTop':'0px','maxHeight':'','minHeight':'','textAlign':'left','fontFamily':'Montserrat-SemiBold,Montserrat','fontWeight':'normal','lineHeight':'0.9476249999999998','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','textDecoration':'none'}"/>
+        <WLabel top="45%" left="54.785714285714285%" text="Bla bla bla bla" v-bind:snapX="false" v-bind:snapY="false"
+          theme="theme--light01" position="absolute" ncTypography="custom" ncTooltipText=""
+          v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':false,'structure':[{'type':'string','propName':'text','displayName':'Value'}]}"
+          id="c1dd3e7d-6276-4118-83c1-0377e94ca371"
+          v-bind:style="{'color':'#808080','width':'293.3125px','height':'49.637499999999996px','zIndex':11,'fontSize':'14.891249999999998px','maxWidth':'','minWidth':'','fontStyle':'normal','marginTop':'0px','maxHeight':'','minHeight':'','textAlign':'left','fontFamily':'Roboto-Regular,Roboto','fontWeight':'normal','lineHeight':'0.9476249999999998','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','textDecoration':'none'}"/>
+        <WLabel top="58.45679389105903%" left="54.7569465637207%" text="Image frrom Nucal" v-bind:snapX="false"
+          v-bind:snapY="false" theme="theme--light01" position="absolute" ncTypography="custom" ncTooltipText=""
+          v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':false,'structure':[{'type':'string','propName':'text','displayName':'Value'}]}"
+          id="4fd8c6e0-e4b6-4d06-bf3f-ae79498beeb5"
+          v-bind:style="{'color':'#808080','width':'','height':'','zIndex':12,'fontSize':'12.183749999999998px','maxWidth':'','minWidth':'','fontStyle':'normal','marginTop':'0px','maxHeight':'','minHeight':'','textAlign':'left','fontFamily':'Roboto-Regular,Roboto','fontWeight':'normal','lineHeight':'0.9476249999999998','marginLeft':'0px','paddingTop':'0px','textShadow':'','visibility':'visible','marginRight':'0px','paddingLeft':'0px','marginBottom':'0px','paddingRight':'0px','paddingBottom':'0px','textDecoration':'underline'}"/>
+        <WPillInfoButton tag="button" top="64.66049759476273%" left="54.7569465637207%" v-bind:pill="true" size="md"
+          text="READ MORE" type="button" v-bind:block="false" color="" v-bind:snapX="false" v-bind:snapY="false"
+          theme="theme--light01" v-bind:squared="false" variant="info" position="absolute" v-bind:isEnabled="true"
+          background="#f7743d" borderColor="#17A2B8" ncTypography="custom" v-bind:resetTrigger="false" ncTooltipText=""
+          verticalWidth="60px"
+          v-bind:nucalDataModel="{'type':'NSW','isList':false,'dynamic':false,'structure':[{'type':'string','propName':'text','displayName':'Value'}]}"
+          verticalHeight="60px" horizontalWidth="72.4479px" nucalScaleWidth="" horizontalHeight="37.5347px"
+          ncClickFontColor="#FFFFFF" ncFocusFontColor="#FFFFFF" ncHoverFontColor="#FFFFFF" nucalScaleHeight=""
+          ncClickBorderColor="#4285f4" ncDisableFontColor="#FFFFFF" ncFocusBorderColor="#4285f4"
+          ncHoverBorderColor="#4285f4" ncDisableBorderColor="transparent" ncClickBackgroundColor="#4285f4"
+          ncFocusBackgroundColor="#4285f4" ncHoverBackgroundColor="#4285f4" ncDisableBackgroundColor="#969696"
+          id="77c64e91-d876-4cf1-8589-e1a8ba6e1905"
+          v-bind:style="{'width':'127.25249999999998px','height':'37.904999999999994px','zIndex':13,'fontSize':'10.153125px','maxWidth':'','minWidth':'','tabIndex':'0px','boxShadow':'','fontStyle':'normal','marginTop':'0px','maxHeight':'','minHeight':'','textAlign':'center','fontFamily':'OpenSans-Bold,Open Sans','fontWeight':'bold','lineHeight':'0.9476249999999998','marginLeft':'0px','paddingTop':'','textShadow':'','visibility':'visible','borderStyle':'none','marginRight':'0px','paddingLeft':'','marginBottom':'0px','paddingRight':'','paddingBottom':'','borderTopWidth':'0.6768749999999999px','textDecoration':'none','borderLeftWidth':'0.6768749999999999px','borderRightWidth':'0.6768749999999999px','horizontalHeight':'25.406300062499998px','borderBottomWidth':'0.6768749999999999px','borderTopLeftRadius':'2.7074999999999996px','borderTopRightRadius':'2.7074999999999996px','borderBottomLeftRadius':'2.7074999999999996px','borderBottomRightRadius':'2.7074999999999996px'}"/>
+      </NucGroupElement>
+    </NucGridCell>
+  </NucGridPage>
+</template>
+<script>
+export default {
+  name: 'Screen1',
+  data() {
+    return {};
+  },
+  props: {},
+  created() {},
+  mounted() {},
+  computed: {},
+  methods: {},
+  watch: {},
+};
+</script>
+
+<style scoped></style>
